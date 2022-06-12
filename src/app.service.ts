51039d98923698a1c5752cb08f409ceaa35c6c81
Richard Huáman Gómez <richard21hg92@gmail.com>
2022-06-12 13:37:45 -0500

    se configuro microservicio que reponde a un evento user-created enviado desde una api-principal

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserEvent } from './create-user-event';
 
 @Injectable()
 export class AppService {
+  private readonly analisis: any[] = [];
   getHello(): string {
     return 'Hello World!';
+  }
+
+  resolverCreateUser(data: CreateUserEvent) {
+    console.log('HandlerUserCreate - ANILISIS', data);
+    this.analisis.push({
+      email: data.email,
+      timestamp: new Date(),
+    });
   }
 }
